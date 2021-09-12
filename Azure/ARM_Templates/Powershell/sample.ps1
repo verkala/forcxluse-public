5e4077daf897db347bb8dffb62eedc79aaab63e9
verkala <90492623+verkala@users.noreply.github.com>
2021-09-12 10:10:36 +0530

    #3 - Testing with Try & Catch

--- Azure/ARM_Templates/Powershell/sample.ps1
+++ Azure/ARM_Templates/Powershell/sample.ps1
@@ -1,7 +1,13 @@
-$getVMName = $args[0]
-$getResourceGroup = $args[1]
-$output = "VM Name={0}, and ResourceGroup={1}" -f $getVMName, $getResourceGroup
-Write-Output $output
-$DeploymentScriptOutputs = @{}
-$DeploymentScriptOutputs['text'] = $output
- 
+$OFS = "`r`n"
+try {
+    $getVMName = $args[0]
+    $getResourceGroup = $args[1]
+    $output = $output + $OFS + "VM Name={0}, and ResourceGroup={1}" -f $getVMName, $getResourceGroup
+    $output = $output + $OFS + "Ganesh Verkala"
+    Write-Output $output
+    $DeploymentScriptOutputs = @{}
+    $DeploymentScriptOutputs['text'] = $output
+}
+catch {
+    $output = $output + $OFS + "Error Message: $_"
+}
