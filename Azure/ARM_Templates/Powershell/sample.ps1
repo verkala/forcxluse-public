c17c6a3d4800305ab3d8d9c896d4b3976f549b0d
verkala <90492623+verkala@users.noreply.github.com>
2021-09-12 10:42:31 +0530

    Update sample.ps1

--- Azure/ARM_Templates/Powershell/sample.ps1
+++ Azure/ARM_Templates/Powershell/sample.ps1
@@ -1,13 +1,15 @@
+$getVMName = $args[0]
+$getResourceGroup = $args[1]
 $OFS = "`r`n"
+$DeploymentScriptOutputs = @{}
 try {
-    $getVMName = $args[0]
-    $getResourceGroup = $args[1]
     $output = "VM Name={0}, and ResourceGroup={1}" -f $getVMName, $getResourceGroup
     $output = $output + $OFS + "Ganesh Verkala"
     #Write-Output $output
-    $DeploymentScriptOutputs = @{}
-    $DeploymentScriptOutputs['text'] = $output
 }
 catch {
     $output = $output + $OFS + "Error Message: $_"
+}
+Finally {
+    $DeploymentScriptOutputs['text'] = $output
 }
