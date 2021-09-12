b88aa63d21556db47fe1160b518e043df4d7bd15
verkala <90492623+verkala@users.noreply.github.com>
2021-09-12 10:31:40 +0530

    Update sample.ps1

--- Azure/ARM_Templates/Powershell/sample.ps1
+++ Azure/ARM_Templates/Powershell/sample.ps1
@@ -2,7 +2,7 @@ $OFS = "`r`n"
 try {
     $getVMName = $args[0]
     $getResourceGroup = $args[1]
-    $output = $output + $OFS + "VM Name={0}, and ResourceGroup={1}" -f $getVMName, $getResourceGroup
+    $output = "VM Name={0}, and ResourceGroup={1}" -f $getVMName, $getResourceGroup
     $output = $output + $OFS + "Ganesh Verkala"
     #Write-Output $output
     $DeploymentScriptOutputs = @{}
