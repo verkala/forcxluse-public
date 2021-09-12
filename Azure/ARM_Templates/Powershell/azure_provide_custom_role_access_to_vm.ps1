c862e5af65e0258e34133685e791ecd3c797f227
verkala <90492623+verkala@users.noreply.github.com>
2021-09-12 09:58:33 +0530

    #1 - Testing - w/ $output variable
    $output variable is mandatory for ARM Template

--- Azure/ARM_Templates/Powershell/azure_provide_custom_role_access_to_vm.ps1
+++ Azure/ARM_Templates/Powershell/azure_provide_custom_role_access_to_vm.ps1
@@ -1,11 +1,14 @@
 # Checking for input arguments
 if ($args.Count -ne 2) {
-	Write-Host "ERROR: Invalid arguments, rerun sccript with right arguments"
-	Write-Host "       Usage: <script> VM-Name VM-Resource-Group"
+	$output = $output + $OFS + "ERROR: Invalid arguments, rerun sccript with right arguments"
+	$output = $output + $OFS + "       Usage: <script> VM-Name VM-Resource-Group"
 	Exit
 }
 $getVMName = $args[0]
 $getResourceGroup = $args[1]
+
+# Global variables
+$OFS = "`r`n"
 
 # Get VM details and check for its System assigned managed identity
 try {
@@ -14,19 +17,19 @@ try {
 	#$getResourceGroup = Read-Host "Enter VM Resource Group name"
 	$checkVMExistance = Get-AzVM -ResourceGroupName $getResourceGroup -Name $getVMName -ErrorVariable notPresent -ErrorAction SilentlyContinue
 	if ($notPresent) {
-		Write-Host "ERROR: VM '$getVMName' and/or Resource Group '$getResourceGroup' does not exists. Re-run script with correct details"
+		$output = $output + $OFS + "ERROR: VM '$getVMName' and/or Resource Group '$getResourceGroup' does not exists. Re-run script with correct details"
 		Exit
 	} else {
 		if (Get-AzADServicePrincipal -DisplayName $getVMName) {
-			Write-Host ""
+			$output = $output + $OFS + ""
 		} else {
-			Write-Host "ERROR: System Assigned Managed Identity is not enabled for '$getVMName' VM. Enable VM's System Assigned Managed Identity and re-run the script"
+			$output = $output + $OFS + "ERROR: System Assigned Managed Identity is not enabled for '$getVMName' VM. Enable VM's System Assigned Managed Identity and re-run the script"
 			Exit
 		}
 	}
 }
 catch {
-	Write-Host "Error Message: $_"
+	$output = $output + $OFS + "Error Message: $_"
 }
 
 # Get list of all accessible Subscriptions
@@ -38,7 +41,7 @@ try {
 	}
 }
 catch {
-	Write-Host "Error Message: $_"
+	$output = $output + $OFS + "Error Message: $_"
 }
 
 # Creating the Custom Role at all Subscriptions
@@ -46,7 +49,7 @@ try {
 	$ErrorActionPreference = "Stop"
 	$roleName = "CloudAccel Custom Role"
 	if (Get-AzRoleDefinition -Name $roleName -WarningAction Ignore) {
-		Write-Host "Custom Role '$roleName' already exists."
+		$output = $output + $OFS + "Custom Role '$roleName' already exists."
 	} else {
 		$allReadPermissions = "Microsoft.Compute/disks/read","Microsoft.Network/networkInterfaces/read","Microsoft.Network/publicIPAddresses/read","Microsoft.Compute/virtualMachines/read","Microsoft.Network/networkInterfaces/join/action","Microsoft.ChangeAnalysis/resourceChanges/*","Microsoft.Insights/Metrics/Read","Microsoft.Subscription/aliases/read","Microsoft.Resources/subscriptions/resourceGroups/read"
 		$customRole = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
@@ -68,13 +71,13 @@ try {
 			$roleVar = Get-AzRoleDefinition -Name $customRole.Name -WarningAction Ignore
 			if ([string]::IsNullOrEmpty($roleVar)) {
 				$roleVar = New-AzRoleDefinition -Role $customRole
-				Write-Host "Custom Role '$roleName' created successfully"
+				$output = $output + $OFS + "Custom Role '$roleName' created successfully"
 			}
 		}
 	}
 }
 catch {
-	Write-Host "Error Message: $_ "
+	$output = $output + $OFS + "Error Message: $_ "
 }
 
 # Assign custom role to given VM at all Subscriptions. Refer to https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles for build in roles
@@ -85,18 +88,18 @@ try {
 		$getVMSAMIID = (Get-AzADServicePrincipal -DisplayName $getVMName).Id
 		foreach ($subscription in $scope) {
 			$assignCustomRole = New-AzRoleAssignment -Scope $subscription -ObjectId $getVMSAMIID -RoleDefinitionName $roleName
-			Write-Host "'$roleName' Role access assigned to $getVMName VM at $subscription level"
+			$output = $output + $OFS + "'$roleName' Role access assigned to $getVMName VM at $subscription level"
 			#Write-Host "Sub - $subscription; RoleID - $getVMSAMIID; RoleName - $roleName"
 			# Monitoring Reader Role
 			$assignMonitorRole = New-AzRoleAssignment -Scope $subscription -ObjectId $getVMSAMIID -RoleDefinitionId "43d0d8ad-25c7-4714-9337-8ba259a9fe05"
-			Write-Host "'Monitoring Reader' Role access assigned to $getVMName VM at $subscription level"
+			$output = $output + $OFS + "'Monitoring Reader' Role access assigned to $getVMName VM at $subscription level"
 		}
 	} else {
-		Write-Host "'$roleName' Role does not exists"
+		$output = $output + $OFS + "'$roleName' Role does not exists"
 	}
 }
 catch {
-	Write-Host "Error Message: $_"
+	$output = $output + $OFS + "Error Message: $_"
 }
 
 # Attach Reader Role to VM at each Reservation Order ID
@@ -106,18 +109,23 @@ try {
 	$getVMSAMIID = (Get-AzADServicePrincipal -DisplayName $getVMName).Id
 	$Reservation_Order = Get-AzReservationOrder
 	$res_count = 0
-	Write-Host ""
+	$output = $output + $OFS + ""
 	foreach ($Reservation in $Reservation_Order) {
 		if ($Reservation.ProvisioningState -eq 'Succeeded') {
 			++$res_count
 			$ReservationOrderID = $Reservation.Id
 			$reservationStatus = $Reservation.ProvisioningState
 			$assignReservationIDReaderRole = New-AzRoleAssignment -Scope $ReservationOrderID -ObjectId $getVMSAMIID -RoleDefinitionName "Reader"
-			Write-Host "$res_count. Reader Role access assigned to $getVMName VM at $ReservationOrderID level"
+			$output = $output + $OFS + "$res_count. Reader Role access assigned to $getVMName VM at $ReservationOrderID level"
 		}
 	}
 }
 catch {
-	Write-Host "Current logged in user do not have privileges to provide ""Reserved pricing orders"" read access to azuretenants@cloudaccel.io. Please login to Azure portal using user having tenant owner privileges and rerun steps mentioned "'Steps in details'"."
+	$output = $output + $OFS + "Current logged in user do not have privileges to provide ""Reserved pricing orders"" read access to azuretenants@cloudaccel.io. Please login to Azure portal using user having tenant owner privileges and rerun steps mentioned "'Steps in details'"."
 	#Write-Host "Error Message:  $_ "
 }
+
+# Output to console
+Write-Output $output
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $output
